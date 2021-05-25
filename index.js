e4f4f54e5ce9072f12a2f17aa13e0d0ff128c7b4
Mark Manipula <markmikomanipula@gmail.com>
2021-05-25 09:59:52 -0700

    using travel advisor api insead

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 const express = require('express')
 const app = express()
 
+//require cors so front end can to to our backend
+const cors = require('cors')
+app.use(cors())
+
 //require fetch
 const fetch = require('node-fetch')
 
 //use json so we can access body
 app.use(express.json())
+
+//require unirest for travel advisor api
+const unirest = require("unirest");
 
 //password is in a .env file
 require('dotenv').config()
@@ -30,28 +37,88 @@ MongoClient.connect(`${connection_string}`, { useUnifiedTopology: true }, (err, 
       if (err) return console.error(err)
 
       const db = client.db(`${databaseName}`)
+
+      //this two are test
       const destinationsCollection = db.collection('destinations')
       const locationsCollection = db.collection('locations')
 
+      //this will hold some data out of the 20 pictures that the user likes
+      const firstReviewLikes = db.collection('firstReviewLikes')
+
+      //this will hold 0-5 pictures out of the initialUserLikes pictrues
+      const secondReviewLikes = db.collection('secondReviewLikes')
+
+      //last photo that the user will get info
+      const finalReviewLikes = db.collection('finalReviewLikes')
 
       //Listen to the port. this has to be first
       app.listen(PORT, () => {
             console.log(`Connected to Database at PORT:${PORT}`);
       })
 
-
       //has to be async so we can get the data from namesCollection
-      app.get("/test", async (req, res) => {
+      app.get("/test", (req, res) => {
             // const outputNames = namesCollection.find().toArray()
             // const outputLocations = locationsCollection.find().toArray()
-
-            const output = await locationsCollection.find().toArray()
-            console.log(output)
             res.send({ status: 'check!' })
       })
 
-      //
-      app.post("/test", (req, res) => {
+      //this will be called for the first review
+      app.post("/firstReview", (req, res) => {
+
+            //namesCollection.insertOne(req.body)
+            const { destination, location } = req.body
+
+            //runirest api request for travel advisor
+            req = unirest("GET", "https://travel-advisor.p.rapidapi.com/locations/search");
+
+            req.query({
+                  "query": `${destination} ${location}`,
+                  "limit": "30",
+                  "offset": "0",
+                  "units": "mi",
+                  "location_id": "1",
+                  "currency": "USD",
+                  "sort": "relevance",
+                  "lang": "en_US"
+            });
+
+            req.headers({
+                  "x-rapidapi-key": `${process.env.API_KEY_TRAVEL}`,
+                  "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+                  "useQueryString": true
+            });
+
+            req.end(function (res) {
+                  if (res.error) throw new Error(res.error);
+
+                  //checking api with travel advisor.
+                  //looking at things to do for a result type
+                  let array = res.body.data
+
+                  let outputArray = []
+
+                  //puts array in the out ouputArray that has things to do, so we filter out lodging and stuff
+                  for (let i = 0; i < array.length; i++) {
+                        if (array[i].result_type === "things_to_do") {
+                              outputArray.push(array[i])
+                        }
+                  }
+
+                  const picURL = outputArray[0].result_object.photo.images.original.url
+
+                  firstReviewLikes.insertOne({
+                        destination: destination,
+                        location: location,
+                        picture: picURL
+                  })
+            });
+            res.send({ status: 'Submitted' })
+      })
+
+      //this will be called for the second review 
+      //but adding it to the second review collection
+      app.post("/secondReview", (req, res) => {
 
             //namesCollection.insertOne(req.body)
             const { destination, location } = req.body
@@ -62,7 +129,7 @@ MongoClient.connect(`${connection_string}`, { useUnifiedTopology: true }, (err, 
 
                   const picURL = picture.results[0].urls.thumb
 
-                  destinationsCollection.insertOne({
+                  secondReviewLikes.insertOne({
                         destination: destination,
                         location: location,
                         picture: picURL
@@ -71,6 +138,29 @@ MongoClient.connect(`${connection_string}`, { useUnifiedTopology: true }, (err, 
             res.send({ status: 'Submitted' })
       })
 
+      //this will add to the final review
+      //only going to be one picture
+      app.post("/finalReview", (req, res) => {
+
+            //namesCollection.insertOne(req.body)
+            const { destination, location } = req.body
+
+            //getting api request through node-fetch
+            fetch(`${unsplashURL}${destination} ${location}`).then((response) => response.json()).then((picture) => {
+                  //namesCollection.insertOne()
+
+                  const picURL = picture.results[0].urls.thumb
+
+                  finalReviewLikes.insertOne({
+                        destination: destination,
+                        location: location,
+                        picture: picURL
+                  })
+            })
+            res.send({ status: 'Submitted' })
+      })
+
+      //deletes an item with a given ID
       app.delete("/test/:id", (req, res) => {
 
             destinationsCollection.deleteOne({ "_id": ObjectID(req.params.id) })
@@ -78,6 +168,7 @@ MongoClient.connect(`${connection_string}`, { useUnifiedTopology: true }, (err, 
             res.send({ status: "deleted" })
       })
 
+      //edits an item with a given ID
       app.put("/test/:id", (req, res) => {
 
             const { destination, location } = req.body
@@ -94,7 +185,6 @@ MongoClient.connect(`${connection_string}`, { useUnifiedTopology: true }, (err, 
                               picture: picURL
                         }
                   })
-
             })
             res.send({ status: 'updated' })
       })
